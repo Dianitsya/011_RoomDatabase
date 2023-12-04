5f70174e46c41b367806363a3632d955e59ed591
Dianitsya <dianitasyanandamasta@gmail.com>
2023-12-04 22:55:50 +0700

    import class Aplikasi siswa

--- app/src/main/java/com/example/roomsiswaa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswaa/model/PenyediaViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswaa.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -23,4 +24,4 @@ object PenyediaViewModel {
  * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari
  * [AplikasiSiswa].
  */
-fun CreationExtras.aplikasiSiswa():AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
